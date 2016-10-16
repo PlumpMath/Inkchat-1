cdc5813cf7c5c2c4c553c79bdeb30cb6b909c710
tidle <tidle@users.noreply.github.com>
2016-10-16 10:51:46 -0700

    2.0

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
  
-var io = require("socket.io-client")("http://10.0.0.8:3000");
+var io = require("socket.io-client")("http://localhost:3000");
 //var socket = io();
  /*     $('form').submit(function(){
         socket.emit('chat message', $('#m').val());
@@ -17,18 +17,28 @@ var io = require("socket.io-client")("http://10.0.0.8:3000");
      });
      var i = 0;
      var name="";
-     console.log("name:")
+     process.stdout.write("name:")
      rl.on("line", function(ln) {
-	     i++;
+	    	     i++;
 	     if (i>1) {
 	     	io.emit("my other event", name+": "+ln);
 	     } else {
 	     	name=ln;
 	     }
+	     process.stdout.write(name+": ")
+
 
      });
      	io.on("news", function(dat) {
-		console.log(dat);
+		var q = dat.match(/^\w+\:/);
+		if (!q){
+			console.log("WARN:no name found");
+			//console.log(dat)
+		}else if(q[0] != name+":"){
+			console.log("\r"+dat);
+	     		process.stdout.write(name+": ")
+			
+		}
       //io.emit("my other event","I loged on");
 		});
 
